9003f24bee6ac08a3d8c87535cc8b6fab4772f6d
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-11-27 18:17:28 +0200

    Add logo

--- source/conf.py
+++ source/conf.py
@@ -61,7 +61,7 @@ todo_include_todos = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_logo = 'img/Aalto-logo.png'
 html_title = ""
 
 html_theme_options = {
